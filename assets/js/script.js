5bb6d76e8749b987c02febbe8e166acdc07eb59c
rashdogg74 <pauloriordan74@gmail.com>
2021-11-20 19:12:22 +0000

    add comments to indicate rungame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the Dom to finish loading before running the game
+// Get the button elements and add event listers to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked Submit!");
+        } else {
+            let gameType = this.getAttribute("data-Type");
+            alert(`You clicked ${gameType}`)
+        }
+        })
+    }
+})
+
 function runGame() {
 
 }
